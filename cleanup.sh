bf75cd808094d00890aa4e5911ed4b1310c4f09c
slow <slow@slow-samsung.(none)>
2013-10-14 16:49:36 -0300

    Hashtags are no longer case sensitive; Added top tweets list.

---
+++ cleanup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+rm tweets_FIXED.csv
+rm -f *.pyc
+mkdir -p results
+mv urls.csv results
+mv dates.csv results
+mv mentions.csv results
+mv hashtags.csv results
+mv locations.csv results
+mv top_tweets.csv results
+mv users_activity.csv results
+mv words_per_period.csv results
+mv top_words_wordle.txt results
+mv top_hashtags_wordle.txt results
+
+
