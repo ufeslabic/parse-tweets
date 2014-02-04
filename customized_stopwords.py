53f78bd9f9634834613e5ff18d3fb47ec8399090
marcusleite <leite.marcus@gmail.com>
2014-02-04 00:23:03 -0200

    Added a lot of stopwords.

--- customized_stopwords.py
+++ customized_stopwords.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+""" 
+This module contains a constantly changing list of stopwords 
+to be used by the script. Feel free to add more words relevant 
+to your needs(or dataset).
+"""
 
-# Feel free to add more words here
-portuguese_common_words = ["o", "a", "os", "as", "ao", "aos", "de", "do", "da", "dos", "das",
-"eu", "me", "mim", "meu", "minha","seu","sua","dele","dela","ele","ela","eles", "elas"
-"lhe","se", "pra", "um", "uma", 
-"esse", "essa", "este", "esta", 
-"aquele", "aquela", "com", 
-"que", "quê", "cujo", "cuja", 
-"no", "na", "nos", "nas", "em", "sobre"
-"ser", "estar", "é", "eh", "ir", "foi", "no", "na", "em", "sim", "ter"
-"de", "do", "da", "por", "pôr", "para", "tem", "dum", "duma", "dela", "dele"
-"para", "por", "isso", "pro", "pra", "para", "isto", "isso", "devemos", "tomem",
-"rt",
-"vc", "ce", "cê",
-"ja", "já", "jah", 
-"só", "soh", 
-"lá", "la", "lah", 
-"se", "sem",
-"até", "ateh", 
-"ta", "tá", "tah", 
-"diz","fala",
-"via", "rt",
-"á", "à", "são", "vão", "vai", "vou", "sobre",
-"não", "naum", "nao", 
-"está", "estah", "esta",
-"mas", "mais", "relnofollowfacebooka", "hrefhttpwwwfacebookcomtwitter", "relnofollowtwitter", "for", "hrefhttptwittercomdownloadandroid", "aug", "hrefhttptwittercomdownloadiphone"]
+portuguese_common_words = ['a', 
+'acha', 'acho', 'ae', 'aeh', 'ah', 'ai', 'aih', 'aih', 'ainda', 
+'algo', 'ao', 'aos', 'após', 'aquela', 'aquele', 'aqui', 'as', 
+'ateh', 'até', 'aug', 'axovc', 'aé', 'aê', 'aí', 'aí', 
+'cada', 'com', 'contudo', 'cuja', 'cujo', 'cê', 
+'da', 'da', 'dah', 'daki', 'daqi', 'daqui', 'das', 'de', 
+'de', 'dela', 'dela', 'dele', 'delepara', 'dessa', 'desse', 
+'desta', 'deste', 'devemos', 'disso', 'diz', 'do', 
+'dos', 'dum', 'duma', 'durante', 'dá', 'eh', 'eh', 'ei', 
+'ela', 'ela', 'elaslhe', 'ele', 'ele', 'ele', 'eles', 
+'em', 'em', 'enfim', 'entao', 'entaum', 'entaun', 'entretanto', 
+'então', 'era', 'essa', 'esse', 'esta', 'esta', 'estah', 'estao', 
+'estar', 'este', 'está', 'estão', 'eu', 'fala', 'faz', 'fazer', 
+'foi', 'for', 'fosse', 'ha', 'ha ha ha', 'he he he', 'hi hi hi', 
+'ho ho ho', 'hora', 'há', 'ia', 'ir', 'iria', 'irá', 'isso', 'isto', 
+'iá', 'ja', 'jah', 'já', 'já', 'la', 'lah', 'lhe', 'lá', 'lá', 
+'mais', 'manda', 'mas', 'me', 'mesmo', 'meu', 'mim', 'minha', 
+'mt', 'mto', 'mtu', 'na', 'nao', 'nas', 'naum', 'neh', 'nesta', 'no', 
+'noix', 'nos', 'nova', 'num', 'não', 'né', 'nóis', 'nós', 'o', 'oi', 
+'onde', 'os', 'ou', 'para', 'para', 'pela', 'pelo', 'pois', 'por', 
+'por', 'portanto', 'pq', 'pra', 'pra', 'pro', 'pôr', 'qt', 'quanto', 
+'que', 'que', 'quê', 'relnofollowfacebooka', 'relnofollowtwitter', 
+'rt', 'rt', 'se', 'se', 'sem', 'ser', 'seu', 'seu', 'sim', 'sim', 
+'so', 'sobre', 'sobreser', 'soh', 'soh', 'somos', 'sua', 'sua', 
+'só', 'só', 'ta', 'tabem', 'taben', 'tah', 'tal', 'tambem', 
+'também', 'tamem', 'tamen', 'tanta', 'tanto', 'tava', 'tb', 
+'tbm', 'tchau', 'te', 'tem', 'tem', 'ter', 'terde', 'teu', 
+'teve', 'to', 'todavia', 'todo', 'tomem', 'tu', 'tua', 'tá', 
+'tô', 'tô', 'um', 'uma', 'umas', 'uns', 'vai', 'vamo', 'vamos', 
+'vc', 'vcs', 'veio', 'vez', 'via', 'vier', 'vim', 'vin', 
+'vir', 'voces', 'vocês', 'vou', 'vão', 'às', 'ás']
