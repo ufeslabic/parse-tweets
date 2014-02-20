e13a23801a3f8cc3d8f0bf1e4b97cbd798496e08
marcusleite <leite.marcus@gmail.com>
2014-02-20 11:29:34 -0300

    Changed to create a gephi compatible output.

--- hashtags_network.py
+++ hashtags_network.py
@@ -10,6 +10,8 @@ import itertools
 from lib_text import remove_invalid_characters
 from lib_output import DEFAULT_OUTPUT_DELIMITER
 
+GEPHI_DELIMITER=';'
+
 def get_hashtags(str_text):
 	""" 
 	Returns all the hashtags in a given string. 
@@ -30,17 +32,17 @@ def process_hashtags_relations(str_tweet_text):
 	list_str_hashtags = get_hashtags(str_tweet_text)
 	list_hashtags_combinations = []
 	for item in itertools.combinations(list_str_hashtags, 2):
-		list_hashtags_combinations.append([item[0], item[1]])
+		list_hashtags_combinations.append([item[0], item[1]])	
 	return(list_hashtags_combinations)
 
-def hashtags_relations_to_csv(list_tuple_hashtags, filename='hashtags_network.csv'):
+def hashtags_relations_to_csv(list_tuple_hashtags, filename='hashtags_network.csv'):	
 	"""
 	Generates a file to be used by Gephi to create a graph of 
 	the relations between the hashtags.	
-	"""
-	with open(filename, 'w', newline='', encoding="utf8") as csvfile:
-		file_writer = csv.writer(csvfile, delimiter=DEFAULT_OUTPUT_DELIMITER, quotechar='"', quoting=csv.QUOTE_MINIMAL)
-		file_writer.writerow(['hashtag_1', 'hashtag_2'])
+	"""	
+	with open(filename, 'w', newline='', encoding="utf8") as csvfile:		
+		file_writer = csv.writer(csvfile, delimiter=GEPHI_DELIMITER, quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		file_writer.writerow(['source', 'target'])
 		for hashtag_tuple in list_tuple_hashtags:
 			file_writer.writerow([hashtag_tuple[0], hashtag_tuple[1]])
 		csvfile.close()
